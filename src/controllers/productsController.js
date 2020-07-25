67ac3091d683fb2cae2057038b85a07279705e80
juanicardozo <j.i.cardozo@hotmail.com>
2020-07-24 23:26:43 -0300

    Se agrega CRUD Productos y se hacen modificaciones en HTML

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,13 +1,136 @@
+const fs = require('fs');
+const path = require('path');
+
+let productos;
+
+
+
+
 const productsController =
 {
     products: function(req,res){
-        res.render('products')
+        productos = fs.readFileSync(path.join(__dirname, '../data/products.json'), 'utf8');
+        productos = JSON.parse(productos);
+
+        res.render('products', {producto: productos });
     },
     detailProduct: function(req,res){
-         res.render('detailProduct')
+
+        productos = fs.readFileSync(path.join(__dirname, '../data/products.json'), 'utf8');
+        productos = JSON.parse(productos);
+
+        console.log('ENTRE A DETALLE PRODUCTOOOOOOOOOOOOOO');
+        console.log('ID PRODUCTO ' + req.params.idProducto);
+        console.log(productos);
+        console.log(productos[req.params.idProducto])
+        res.render('detailProduct', {producto: productos[req.params.idProducto]})
     },
     newProduct: function(req,res){
         res.render('newProduct')
+    },
+    createProduct: function(req,res){
+
+        productos = fs.readFileSync(path.join(__dirname, '../data/products.json'), 'utf8');
+        productos = JSON.parse(productos);
+
+        let ultimoID = productos.length
+
+        let nuevoProducto = {
+            id: ultimoID + 1,
+            name: req.body.name,
+            price: req.body.price,
+            category: req.body.category,
+            color: req.body.color,
+            image: 'imagenPrueba',
+            description: req.body.description,
+            stock: req.body.stock
+        }
+
+        productos.push(nuevoProducto);
+        productos = JSON.stringify(productos);
+
+        fs.writeFileSync(path.join(__dirname, '../data/products.json'), productos);
+
+        return res.redirect('productOK');
+    },
+    newProductOK: function(req,res){
+        res.render('productOK');
+    },
+    editProduct: function(req,res){
+
+        /*for(let i = 0; i < productos.length; i++) {
+			if(productos[i].id == req.params.idProducto) {
+				return res.render('editProduct', {
+					producto: productos[i]
+				});
+			}
+        }*/
+        productos = fs.readFileSync(path.join(__dirname, '../data/products.json'), 'utf8');
+        productos = JSON.parse(productos);
+
+        console.log('ENTRE A EDITAR PRODUCTO');
+        console.log(req.params.idProducto);
+        console.log(productos[req.params.idProducto]);
+
+        res.render('editProduct', {producto: productos[req.params.idProducto]});
+		//res.send('Ese ID no capo');
+    },
+    modifyProduct: function(req,res){
+
+        productos = fs.readFileSync(path.join(__dirname, '../data/products.json'), 'utf8');
+        productos = JSON.parse(productos);
+
+        console.log('Estoy en el PUT');
+        console.log(productos[req.params.idProducto]);
+        console.log(req.params.idProducto);
+        console.log(req.body);
+        //productos[req.params.idProducto - 1].id = Number.parseInt(req.body.idProducto);
+        productos[req.params.idProducto].name = req.body.name;
+        productos[req.params.idProducto].price = req.body.price;
+        productos[req.params.idProducto].category = req.body.category;
+        productos[req.params.idProducto].color = req.body.color;
+        productos[req.params.idProducto].image = 'imagenPrueba';
+        productos[req.params.idProducto].description = req.body.description;
+        productos[req.params.idProducto].stock = req.body.stock;
+
+        //productos[req.params.idProducto - 1] = req.body;
+        console.log(productos[req.params.idProducto]);
+
+        productos = JSON.stringify(productos);
+
+        fs.writeFileSync(path.join(__dirname, '../data/products.json'), productos);
+
+        res.redirect('/products/productEditOK');
+    },
+    editProductOK: function(req,res){
+        res.render('productEditOK');
+    },
+    confirmDeleteProduct: function(req,res){
+
+        productos = fs.readFileSync(path.join(__dirname, '../data/products.json'), 'utf8');
+        productos = JSON.parse(productos);
+
+        res.render('deleteProduct', {producto: productos[req.params.idProducto]});
+    },
+    deleteProduct: function(req,res){
+
+        productos = fs.readFileSync(path.join(__dirname, '../data/products.json'), 'utf8');
+        productos = JSON.parse(productos);
+
+        console.log('Estoy en el DELETE');
+        console.log(productos[req.params.idProducto]);
+        console.log(req.params.idProducto);
+        console.log(req.body);
+
+        productos.splice(req.params.idProducto,1);
+
+        productos = JSON.stringify(productos);
+        fs.writeFileSync(path.join(__dirname, '../data/products.json'), productos);
+
+        res.redirect('/products/productDeleteOK');
+    },
+    deleteProductOK: function(req,res){
+        res.render('productDeleteOK');
     }
 }
 
--- src/routes/products.js
+++ src/routes/products.js
@@ -2,8 +2,23 @@ const express = require('express');
 const router = express.Router();
 const productsController = require('../controllers/productsController');
 
+/* Listado de productos */
 router.get('/', productsController.products);
-router.get('/detailProduct', productsController.detailProduct);
+router.get('/detailProduct/:idProducto', productsController.detailProduct);
+
+/* Alta de productos */
 router.get('/newProduct', productsController.newProduct);
+router.post('/newProduct', productsController.createProduct);
+router.get('/productOK', productsController.newProductOK);
+
+/* Modificacion de productos */
+router.get('/editProduct/:idProducto', productsController.editProduct);
+router.put('/editProduct/:idProducto', productsController.modifyProduct);
+router.get('/productEditOK', productsController.editProductOK);
+
+/* Baja de productos */
+router.get('/deleteProduct/:idProducto', productsController.confirmDeleteProduct);
+router.delete('/deleteProduct/:idProducto', productsController.deleteProduct);
+router.get('/productDeleteOK', productsController.deleteProductOK);
 
 module.exports = router;
